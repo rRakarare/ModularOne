75568bbeb66e52d737fd808243d09d68dbb05fa4
rRakarare <rRene333@googlemail.com>
2023-03-23 07:12:59 +0100

    vscroll

---
+++ components/New/CardRow.js
@@ -0,0 +1,58 @@
+import { Box, Card, CardBody, Text } from "@chakra-ui/react";
+import { useLenis } from "@studio-freight/react-lenis";
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+
+function CardRow() {
+
+    const [x, setX] = useState(0);
+
+    const lenis = useLenis((state, a, b, c) => {
+      console.log(state.progress);
+      setX(-state.progress*2000);
+    });
+
+  return (
+    <Box position={"sticky"} top={"40vh"}>
+      <Box overflow={"hidden"} width={"90vw"} position={"relative"} display={"block"} height={"400px"}>
+        <Card
+          animate={{ x }}
+          as={motion.div}
+          position={"absolute"}
+          left={"100vw"}
+          width={300}
+          height={600}
+        >
+          <CardBody>
+            <Text>
+              View a summary of all your customers over the last month.
+            </Text>
+          </CardBody>
+        </Card>
+        <Card position={"absolute"} left={"100vw"} width={300} height={600}>
+          <CardBody>
+            <Text>
+              View a summary of all your customers over the last month.
+            </Text>
+          </CardBody>
+        </Card>
+        <Card position={"absolute"} left={1000} width={300} height={600}>
+          <CardBody>
+            <Text>
+              View a summary of all your customers over the last month.
+            </Text>
+          </CardBody>
+        </Card>
+        <Card position={"absolute"} left={1500} width={300} height={600}>
+          <CardBody>
+            <Text>
+              View a summary of all your customers over the last month.
+            </Text>
+          </CardBody>
+        </Card>
+      </Box>
+    </Box>
+  );
+}
+
+export default CardRow;
--- components/New/index.js
+++ components/New/index.js
@@ -1,49 +1,57 @@
-import { Box, Container, Heading, Text, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  Card,
+  CardBody,
+  Container,
+  Heading,
+  HStack,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import { ReactLenis, useLenis } from "@studio-freight/react-lenis";
+import { motion } from "framer-motion";
 import React, { useEffect, useState } from "react";
 import HeroHome from "../HeroHome";
+import CardRow from "./CardRow";
 
 function New() {
-  const lenis = useLenis((state, a, b, c) => {
-    // console.log(state.animatedScroll);
-  });
+
 
   return (
-    <ReactLenis root options={{
-      lerp:0.05
-    }}>
+    <ReactLenis
+      root
+      options={{
+        lerp: 0.05,
+      }}
+    >
       <HeroHome />
-      <Box height={"100vh"}/>
+      <Box height={"100vh"} />
       <Box mx={40}>
-      <Heading position={"sticky"} top={"40vh"} fontSize={"8rem"}>
-        Modular <Text color={"yellow.300"}>One</Text>
-      </Heading>
-      <VStack px={20} width={"100%"} mt={"100vh"}>
-        <Box
-          width={"100%"}
-          height={"800px"}
-          display="flex"
-          alignItems={"space-between"}
-        >
-          <Box width={"70%"}></Box>
-          <Box width={"30%"}>
-            <Heading>
-              Bruh iceland iPhone subway tile pitchfork waistcoat. Yr lyft food
-              truck try-hard. Fashion axe flexitarian edison bulb narwhal
-              iceland shoreditch lo-fi franzen post-ironic cray la croix blog
-            </Heading>
+        <Heading position={"sticky"} top={"40vh"} fontSize={"8rem"}>
+          Modular <Text color={"yellow.300"}>One</Text>
+        </Heading>
+        <VStack px={20} width={"100%"} mt={"100vh"}>
+          <Box
+            width={"100%"}
+            height={"800px"}
+            display="flex"
+            alignItems={"space-between"}
+          >
+            <Box width={"70%"}></Box>
+            <Box width={"30%"}>
+              <Heading>
+                Bruh iceland iPhone subway tile pitchfork waistcoat. Yr lyft
+                food truck try-hard. Fashion axe flexitarian edison bulb narwhal
+                iceland shoreditch lo-fi franzen post-ironic cray la croix blog
+              </Heading>
+            </Box>
           </Box>
-        </Box>
-        <Box height={"800px"}>
-          <Heading>:)</Heading>
-        </Box>
-        <Box height={"800px"}>
-          <Heading>:/</Heading>
-        </Box>
-        <Box height={"800px"}>
-          <Heading>:(</Heading>
-        </Box>
-      </VStack>
+          <Box height={"800px"}>
+            <Heading>:)</Heading>
+          </Box>
+          <CardRow />
+          <Box height={"3400px"}></Box>
+        </VStack>
       </Box>
     </ReactLenis>
   );
